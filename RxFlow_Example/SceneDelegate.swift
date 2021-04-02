5c9a3f88555b86a21870f464bc0adc68bd27956e
Zedd0202 <ch41150@hanmail.net>
2021-04-02 19:07:00 +0900

    disposeBag일단 필요없으니 제거.

--- RxFlow_Example/SceneDelegate.swift
+++ RxFlow_Example/SceneDelegate.swift
@@ -13,7 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var coordinator = FlowCoordinator()
-    var disposeBag: DisposeBag = DisposeBag()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
